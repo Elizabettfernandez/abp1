4c28f21a62875a9b2af13975da46ac17277216b5
elizabettfernandez <e.fernandezallende@gmail.com>
2022-05-14 01:50:35 -0400

    .

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,32 @@
 <template>
+<!-- <div class="container mt-3">
+  <div class="grid">
+    <div class="row">
+      <div class="col">
+        <p class="h3 text-success fw-bold">Lista de usuarios</p>
+          
+        </div>
+      </div>
+    </div>
+ </div> -->
 
-  <TablaUsuarios/>
-    <router-view/>
+
+<Tabla/>
 </template>
 
 
 <script>
-
-  import Tabla from './components/TablaUsuarios.vue'
+  import Tabla from './components/Tabla.vue'
   
   export default {
   name: 'App',
   components: {
-    TablaUsuarios
+    Tabla
   }
-  }
+  };
   
 </script>
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-// nav {
-//   padding: 30px;
+<style>
 
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
 </style>
---
+++ src/components/Tabla.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="container mt-4">
+  <h1>Tabla de usuarios</h1>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">N°</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">Fecha de nacimiento</th>
+      <th scope="col">Edad</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>{{this.usuarios.nombre}}</td>
+      <td>{{this.usuarios.apellido}}</td>
+      <td>{{this.usuarios.fechadenacimiento}}</td>
+      <td>{{this.usuarios.edad}}</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'Tabla',
+      data(){
+        return {
+        usuarios : 
+          {
+             nombre : 'Miguel',
+             apellido : 'Fernández',
+             fechadenacimiento : '16/12/1999',
+             edad : 23,
+            },
+        }  
+      },
+      
+};
+
+</script>
+
+<style>
+h1{
+  text-align: center;
+}
+</style>
