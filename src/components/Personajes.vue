0954a1ed8cd0c5169f2a76047f805470ca359f6b
elizabettfernandez <e.fernandezallende@gmail.com>
2022-05-15 15:40:55 -0400

    .

---
+++ src/components/Personajes.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="container mt-5 mb-5">
+    <div class="card shadow" style="max-width: 18rem" display flex>
+    <div class="card-body" v-for="item in personajes" :key="item.id">
+    <h5 class="card-title">{{item.nombre}}</h5>
+    <p class="card-text">{{item.descripcion}}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Personajes',
+    data(){
+        return {
+        personajes:[
+            {
+            "id": "1",
+            "nombre": "pikachu",
+            "descripcion": "lorem ipsum uhiuhiuhihoi"
+            },
+            {
+            "id": "2",
+            "nombre": "kakuna",
+            "descripcion": "lorem ipsum uhiuhiuhihoi"
+            },
+            {
+            "id": "3",
+            "nombre": "beedrill",
+            "descripcion": "lorem ipsum uhiuhiuhihoi"
+            },
+            {
+            "id": "4",
+            "nombre": "metapod",
+            "descripcion": "lorem ipsum uhiuhiuhihoi"
+            },
+            {
+            "id": "5",
+            "nombre": "rattata",
+            "descripcion": "lorem ipsum uhiuhiuhihoi"
+            }
+        ]
+    };
+
+
+    }
+
+}
+</script>
+
+<style>
+.container{
+    margin-bottom: 5px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,21 @@
  <div class="container">
   <h1>Lista de usuarios</h1>
  <Tabla/>
+ <h2>Cards de personajes</h2>
+ <Personajes />
 </div>
 </template>
 
 
 <script>
-import Tabla from '@/components/Tabla.vue'
+import Tabla from '@/components/Tabla.vue';
+import Personajes from '@/components/Personajes.vue'
+
 export default {
   name: "HomeView",
   components: {
-    Tabla
+    Tabla,
+    Personajes
   },
 };
 </script>
